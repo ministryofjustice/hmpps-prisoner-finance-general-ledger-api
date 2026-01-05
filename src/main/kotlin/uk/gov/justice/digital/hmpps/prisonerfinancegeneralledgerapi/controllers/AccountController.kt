82d5925dd4e71678d565d4997429b3e2c0be8cf6
emilyfbgov <emily.bennett@justice.gov.uk>
2026-01-05 16:16:51 +0000

    Account creation controller unit test

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/AccountController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/AccountController.kt
@@ -2,7 +2,15 @@ package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.controllers
 
 import org.springframework.http.ResponseEntity
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.Account
+import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.services.AccountService
+import java.security.Principal
 
-class AccountController {
-  fun createAccount(accountReference: String): ResponseEntity<Account> = ResponseEntity.ok().body<Account>(Account(reference = accountReference, createdBy = "AccountController"))
+class AccountController(
+  private val accountService: AccountService,
+) {
+  fun createAccount(accountReference: String, user: Principal): ResponseEntity<Account> {
+    val account = accountService.createAccount(accountReference, createdBy = user.name)
+
+    return ResponseEntity.ok().body<Account>(account)
+  }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/AccountControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/AccountControllerTest.kt
@@ -1,16 +1,25 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.controllers
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.Account
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.services.AccountService
+import java.security.Principal
+import java.time.LocalDateTime
+import java.util.UUID
+
+private const val TEST_ACCOUNT_REF = "TEST_ACCOUNT_REF"
+private const val TEST_USERNAME = "TEST_USERNAME"
 
 @ExtendWith(MockitoExtension::class)
 class AccountControllerTest {
@@ -18,20 +27,30 @@ class AccountControllerTest {
   lateinit var accountServiceMock: AccountService
 
   @InjectMocks
-  lateinit var accountsController: AccountController
+  lateinit var accountController: AccountController
+
+  lateinit var dummyAccount: Account
+
+  @BeforeEach
+  fun setupDummyAccount() {
+    val dummyUUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
+    val dummyDate = LocalDateTime.of(2025, 12, 25, 0, 0, 0)
+    dummyAccount = Account(reference = TEST_ACCOUNT_REF, createdBy = TEST_USERNAME, uuid = dummyUUID, createdAt = dummyDate)
+  }
 
   @Nested
   inner class CreateAccounts {
-    @Test
-    fun `createAccount should return 200`() {
-      val accountsReference = "TEST-ACCOUNT-REFERENCE"
-      val response: ResponseEntity<Account> = AccountController().createAccount(accountsReference)
-      assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
-      assertThat(response.body?.reference).isEqualTo(accountsReference)
-    }
 
     @Test
     fun `createAccount should call the service correctly`() {
+      whenever(accountServiceMock.createAccount(TEST_ACCOUNT_REF, TEST_USERNAME)).thenReturn(dummyAccount)
+      val mockPrincipal = Mockito.mock<Principal>()
+      whenever(mockPrincipal.name).thenReturn(TEST_USERNAME)
+
+      val response: ResponseEntity<Account> = accountController.createAccount(TEST_ACCOUNT_REF, mockPrincipal)
+
+      assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+      assertThat(response.body).isEqualTo(dummyAccount)
     }
   }
 }
