6b3364b7d34ccccef3cff579ede696f499e875b4
emilyfbgov <emily.bennett@justice.gov.uk>
2026-01-06 15:55:22 +0000

    Error tests for account creation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/AccountController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/AccountController.kt
@@ -1,23 +1,60 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.controllers
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.config.ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.Account
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.models.CreateAccountRequest
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.services.AccountService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 import java.security.Principal
 
+@Tag(name = "Account Controller")
 @RestController
 class AccountController(
   private val accountService: AccountService,
 ) {
-
+  @Operation(summary = "Create a new account")
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "201",
+        description = "Created a new account",
+        content = [Content(mediaType = "application/json")],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized - requires a valid OAuth2 token",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden - requires an appropriate role",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "500",
+        description = "Internal Server Error - An unexpected error occurred.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @SecurityRequirement(name = "bearer-jwt", scopes = [ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW])
+  @PreAuthorize("hasAnyAuthority('$ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW')")
   @PostMapping(value = ["/account"], consumes = [MediaType.APPLICATION_JSON_VALUE])
-  fun createAccount(@RequestBody body: CreateAccountRequest, user: Principal): ResponseEntity<Account> {
-    println(body.accountReference)
+  fun createAccount(@Valid @RequestBody body: CreateAccountRequest, user: Principal): ResponseEntity<Account> {
     val account = accountService.createAccount(body.accountReference, createdBy = user.name)
 
     return ResponseEntity.status(201).body<Account>(account)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/integration/AccountIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/integration/AccountIntegrationTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.integration
 
+import com.fasterxml.jackson.module.kotlin.jsonMapper
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.config.ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW
@@ -16,5 +17,37 @@ class AccountIntegrationTest : IntegrationTestBase() {
       .bodyValue(CreateAccountRequest("TEST_ACCOUNT_REF"))
       .exchange()
       .expectStatus().isCreated
+  }
+
+  @Test
+  fun `createAccount should return 401 without authorisation headers`() {
+    webTestClient.post()
+      .uri("/account")
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(CreateAccountRequest("TEST_ACCOUNT_REF"))
+      .exchange()
+      .expectStatus().isUnauthorized
+  }
+
+  @Test
+  fun `createAccount should return 403 Forbidden when role is incorrect`() {
+    webTestClient.post()
+      .uri("/account")
+      .headers(setAuthorisation(roles = listOf("WRONG_ROLE")))
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(CreateAccountRequest("TEST_ACCOUNT_REF"))
+      .exchange()
+      .expectStatus().isForbidden
+  }
+
+  @Test
+  fun `createAccount should return 400 when sent a malformed body`() {
+    webTestClient.post()
+      .uri("/account")
+      .headers(setAuthorisation(roles = listOf(ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW)))
+      .contentType(MediaType.APPLICATION_JSON)
+      .bodyValue(jsonMapper { "wrong_key" to "wrong_value" })
+      .exchange()
+      .expectStatus().isBadRequest
   }
 }
