9b777e8c3a3fe639d55b7b18cc42b829e33251a9
emilyfbgov <emily.bennett@justice.gov.uk>
2026-01-16 12:07:32 +0000

    Remove unnecessary decorators

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/models/responses/SubAccountResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/models/responses/SubAccountResponse.kt
@@ -1,20 +1,14 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.models.responses
 
-import jakarta.validation.constraints.NotBlank
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.SubAccountEntity
 import java.time.LocalDateTime
 import java.util.UUID
 
 class SubAccountResponse(
-  @field:NotBlank
   val id: UUID,
-  @field:NotBlank
   val reference: String,
-  @field:NotBlank
   val parentAccountId: UUID,
-  @field:NotBlank
   val createdBy: String,
-  @field:NotBlank
   val createdAt: LocalDateTime,
 ) {
 
