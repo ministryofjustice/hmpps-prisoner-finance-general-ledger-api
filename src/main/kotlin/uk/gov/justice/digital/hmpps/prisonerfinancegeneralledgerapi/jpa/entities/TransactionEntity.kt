233af9eeda5c44b17f859588e33193492b62a6af
tristan-madetech <tristan.moss@justice.gov.uk>
2026-01-19 17:07:16 +0000

    WIP

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/jpa/entities/TransactionEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/jpa/entities/TransactionEntity.kt
@@ -2,7 +2,10 @@ package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entitie
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import java.math.BigInteger
 import java.time.LocalDateTime
@@ -33,4 +36,7 @@ class TransactionEntity(
 
   @Column(name = "amount", nullable = false, unique = true)
   val amount: BigInteger = BigInteger.ZERO,
-)
+){
+  @OneToMany(mappedBy = "transactionEntity")
+  val postings = mutableListOf<PostingEntity>()
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/TransactionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/TransactionService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.services
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.PostingEntity
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.SubAccountEntity
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.TransactionEntity
@@ -15,13 +16,16 @@ class TransactionService(
   private val transactionDataRepository: TransactionDataRepository,
   private val postingsDataRepository: PostingsDataRepository,
 ) {
-
+  @Transactional()
   fun createTransaction(reference: String, createdBy: String, description: String, amount: BigInteger, timestamp: LocalDateTime, postings: List<PostingRequest>): TransactionEntity {
+
     val transactionEntity = TransactionEntity(reference = reference, createdBy = createdBy, description = description, amount = amount, timestamp = timestamp)
     transactionDataRepository.save(transactionEntity)
 
     val postingEntities = postings.map { PostingEntity(createdBy = createdBy, createdAt = transactionEntity.createdAt, type = it.type, amount = it.amount, subAccountEntity = SubAccountEntity(id = it.subAccountId), transactionEntity = TransactionEntity(id = transactionEntity.id)) }
     postingsDataRepository.saveAll(postingEntities)
+
+    transactionEntity.postings.addAll(postingEntities)
 
     return transactionEntity
   }
