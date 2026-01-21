e6ba9f14170693cb584f7d802af9735b3e9f0eb4
emilyfbgov <emily.bennett@justice.gov.uk>
2026-01-21 13:31:04 +0000

    Adds create postings

---
+++ src/main/resources/db/migrations/common/V4__Create_Postings.sql
@@ -0,0 +1,20 @@
+CREATE TABLE postings
+(
+    posting_id     UUID                        NOT NULL,
+    created_by     VARCHAR(255)                NOT NULL,
+    created_at     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    type           VARCHAR(255)                NOT NULL,
+    amount         BIGINT                     NOT NULL,
+    sub_account_id UUID                        NOT NULL,
+    transaction_id UUID                        NOT NULL,
+    CONSTRAINT pk_postings PRIMARY KEY (posting_id)
+);
+
+
+ALTER TABLE postings
+    ADD CONSTRAINT FK_POSTINGS_ON_SUB_ACCOUNT FOREIGN KEY (sub_account_id) REFERENCES sub_accounts (sub_account_id);
+
+ALTER TABLE postings
+    ADD CONSTRAINT FK_POSTINGS_ON_TRANSACTION FOREIGN KEY (transaction_id) REFERENCES transactions (transaction_id);
+
+CREATE INDEX index_postings_transactions_id ON postings (transaction_id);
