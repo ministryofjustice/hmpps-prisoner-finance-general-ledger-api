0ecf7de70a37085a79ddfd8b10a4c8852c9eeb4c
tristan-madetech <tristan.moss@justice.gov.uk>
2026-01-30 16:20:56 +0000

    Modified transaction validator to prohibit many to many transactions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/validators/TransactionValidator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/validators/TransactionValidator.kt
@@ -4,13 +4,20 @@ import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.enums.PostingType
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.models.requests.CreateTransactionRequest
+import kotlin.math.min
 
 class TransactionValidator : ConstraintValidator<ValidTransactionRequest, CreateTransactionRequest> {
   override fun isValid(txn: CreateTransactionRequest?, p1: ConstraintValidatorContext?): Boolean {
     if (txn == null) return false
 
-    val totalPostingCredits = txn.postings.filter { it.type == PostingType.CR }.fold(0L, { acc, it -> acc + it.amount })
-    val totalPostingDebits = txn.postings.filter { it.type == PostingType.DR }.fold(0L, { acc, it -> acc + it.amount })
+    val creditPostings = txn.postings.filter { it.type == PostingType.CR }
+    val debitPostings = txn.postings.filter { it.type == PostingType.DR }
+
+    val fewestPostings = min(creditPostings.size, debitPostings.size)
+    if (fewestPostings != 1) return false
+
+    val totalPostingCredits = creditPostings.fold(0L, { acc, it -> acc + it.amount })
+    val totalPostingDebits = debitPostings.fold(0L, { acc, it -> acc + it.amount })
 
     if (totalPostingCredits != txn.amount) return false
     return totalPostingCredits == totalPostingDebits
