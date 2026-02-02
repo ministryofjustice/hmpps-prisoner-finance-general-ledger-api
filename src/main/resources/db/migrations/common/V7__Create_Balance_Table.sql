2fc973c2e8f1e35dafa8dd94e4de3720b34466fd
sam.caine <sam.caine@justice.gov.uk>
2026-02-02 12:28:29 +0000

    established balance entity and migration

---
+++ src/main/resources/db/migrations/common/V7__Create_Balance_Table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE balances
+(
+    balance_id        UUID   NOT NULL,
+    sub_account_id    UUID   NOT NULL,
+    balance_date_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    amount            BIGINT NOT NULL,
+    CONSTRAINT pk_balances PRIMARY KEY (balance_id)
+);
+
+ALTER TABLE balances
+    ADD CONSTRAINT FK_BALANCES_ON_SUB_ACCOUNT FOREIGN KEY (sub_account_id) REFERENCES sub_accounts (sub_account_id);
+
+CREATE INDEX idx_balance_sub_account_id ON balances (sub_account_id);
