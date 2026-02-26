f4198f0e659c4a15a063978090322e35041e3cda
emilyfbgov <emily.bennett@justice.gov.uk>
2026-02-26 10:32:43 +0000

    env string in app (#65)
    * Updates readability of functionality
    
    * Adds env_string to helm deploy for testing environment
    
    * Different way of getting env var
    
    * Different way of getting env var
    
    * Test fix

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/HelloController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/controllers/HelloController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType.TEXT_PLAIN_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
@@ -48,5 +49,13 @@ class HelloController {
   @SecurityRequirement(name = "bearer-jwt", scopes = [ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW])
   @PreAuthorize("hasAnyAuthority('$ROLE_PRISONER_FINANCE__GENERAL_LEDGER__RW')")
   @GetMapping(path = ["/hello"], produces = [TEXT_PLAIN_VALUE])
-  fun hello(user: Principal): ResponseEntity<String> = ResponseEntity.ok().body("Hello ${user.name}!")
+  fun hello(user: Principal): ResponseEntity<String> {
+    val environmentString = System.getenv("ENVIRONMENT") ?: "not-set"
+    log.info("Environment: $environmentString")
+    return ResponseEntity.ok().body("Hello ${user.name}!")
+  }
+
+  private companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
 }
