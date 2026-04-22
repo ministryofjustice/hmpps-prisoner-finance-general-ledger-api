95cf93e2e13e07bee3a561e4103ca0221d885a64
Pietro Convalle <pietro.convalle@madetech.com>
2026-04-22 10:29:35 +0100

    restored postingBalance entity

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/jpa/entities/PostingBalanceEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/jpa/entities/PostingBalanceEntity.kt
@@ -4,8 +4,6 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import java.time.Instant
@@ -18,17 +16,9 @@ class PostingBalanceEntity(
   @Column(name = "posting_balance_id", nullable = false, unique = true)
   val id: UUID = UUID.randomUUID(),
 
-  @ManyToOne(optional = false, fetch = jakarta.persistence.FetchType.LAZY)
-  @JoinColumn(name = "sub_account_id", nullable = false)
-  val subAccountEntity: SubAccountEntity,
-
   @OneToOne(optional = false, fetch = jakarta.persistence.FetchType.LAZY)
-  @JoinColumn(name = "posting_id", nullable = true)
+  @JoinColumn(name = "posting_id", nullable = false)
   val postingEntity: PostingEntity,
-
-  @OneToOne(optional = false, fetch = jakarta.persistence.FetchType.LAZY)
-  @JoinColumn(name = "statement_balance_id", nullable = true)
-  val statementBalanceEntity: StatementBalanceEntity,
 
   @Column(name = "total_sub_account_balance", nullable = false)
   val totalSubAccountBalance: Long = 0,
