6d04e0fe7f6a39fe49690d4f482ad53157a31f1c
Pietro-MadeTech <pietro.convalle@madetech.com>
2026-07-01 11:02:22 +0100

    removed async annotation (#176)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/sqs/LogSqsCalculatedBalanceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/sqs/LogSqsCalculatedBalanceService.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.services.sqs
 
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.LogSqsCalculatedBalances
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.repositories.LogSqsCalculatedBalancesRepository
@@ -9,7 +8,6 @@ import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.reposito
 class LogSqsCalculatedBalanceService(
   private val logSqsCalculatedBalancesRepository: LogSqsCalculatedBalancesRepository,
 ) {
-  @Async
   fun save(log: LogSqsCalculatedBalances) {
     logSqsCalculatedBalancesRepository.save(log)
   }
