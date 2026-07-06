af8198c2d4f1c2ed72b2bc08f92708f4dfc48ec2
Pietro Convalle <pietro.convalle@madetech.com>
2026-07-06 11:00:04 +0100

    Added insights for queue time, adjusted hikari config to add leakConnections

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/ProcessPostingBalanceService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/ProcessPostingBalanceService.kt
@@ -1,16 +1,20 @@
 package uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.services
 
+import com.microsoft.applicationinsights.TelemetryClient
 import jakarta.transaction.Transactional
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.config.TELEMETRY_PREFIX
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.entities.PostingEntity
 import uk.gov.justice.digital.hmpps.prisonerfinancegeneralledgerapi.jpa.repositories.PostingsDataRepository
+import java.time.Instant
 import java.util.UUID
 
 @Service
 class ProcessPostingBalanceService(
   private val postingsDataRepository: PostingsDataRepository,
   private val postingBalanceService: PostingBalanceService,
+  private val telemetryClient: TelemetryClient,
 ) {
   private val log = LoggerFactory.getLogger(this::class.java)
 
@@ -20,8 +24,20 @@ class ProcessPostingBalanceService(
 
     log.debug("Processing posting: ${posting?.id}")
     while (posting != null) {
+      val startTime = Instant.now()
+
       postingBalanceService.calculatePostingBalances(posting = posting)
 
+      log.debug("Successfully processed posting: ${posting.id} for accountId: $accountId in ${Instant.now().toEpochMilli() - startTime.toEpochMilli()}ms")
+      telemetryClient.trackEvent(
+        "$TELEMETRY_PREFIX-calculated-balance-queue-posting-creation-time",
+        mapOf(
+          "postingId" to posting.id.toString(),
+          "accountId" to accountId.toString(),
+          "timeTaken" to "${Instant.now().toEpochMilli() - startTime.toEpochMilli()}ms",
+        ),
+        null,
+      )
       posting = postingsDataRepository.getTheNextAccountPostingOrNull(
         postingId = posting.id,
         accountId = posting.subAccountEntity.parentAccountEntity.id,
