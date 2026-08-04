e751b7ae515a5ca792943d8bce403da38b3be8a3
Pietro Convalle <pietro.convalle@madetech.com>
2026-08-04 12:07:51 +0100

    changed saves to saveAndFlush

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/TransactionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinancegeneralledgerapi/services/TransactionService.kt
@@ -37,10 +37,10 @@ class TransactionService(
       entrySequence = createTxReq.entrySequence,
       legacyTransactionId = createTxReq.legacyTransactionId,
     )
-    transactionDataRepository.save(transactionEntity)
+    transactionDataRepository.saveAndFlush(transactionEntity)
 
     val idempotencyEntityToSave = IdempotencyEntity(idempotencyKey, transactionEntity)
-    idempotencyKeyDataRepository.save(idempotencyEntityToSave)
+    idempotencyKeyDataRepository.saveAndFlush(idempotencyEntityToSave)
 
     val postingEntities = createTxReq.postings.map {
       PostingEntity(
@@ -54,7 +54,7 @@ class TransactionService(
         entrySequence = it.entrySequence,
       )
     }
-    postingsDataRepository.saveAll(postingEntities)
+    postingsDataRepository.saveAllAndFlush(postingEntities)
 
     transactionEntity.postings.addAll(postingEntities)
 
