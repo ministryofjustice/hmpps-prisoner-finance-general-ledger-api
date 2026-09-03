82bb893df703e7e60cdd34644840a43ab248ee07
emilyfbgov <emily.bennett@justice.gov.uk>
2026-09-03 16:21:46 +0100

    update smoke test

--- scripts/K6/smoke-tests.js
+++ scripts/K6/smoke-tests.js
@@ -11,12 +11,20 @@ const httpParams = {
 
 const baseUrl = `http://${domain}`
 
+export const options = {
+    thresholds: {
+        checks: ['rate==1.0'],
+    }
+}
+
 export default function verify_health() {
     const res = http.get(`${baseUrl}/health`, httpParams)
 
     check(res, {
         'status is 200': r => r.status === 200,
         'body is defined': r => r.body !== undefined,
+        'pipeline should fail': _r => false
+    //     this is just to check the pipeline fails properly - will remove after
     })
 
     const body = res.json()
