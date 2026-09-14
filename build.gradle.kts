2972b2806eaa6e468d08029a7b67ef9bfc3a5adf
Pietro Convalle <pietro.convalle@madetech.com>
2026-09-14 13:54:59 +0100

    fixed ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,13 @@ configure<JacocoPluginExtension> {
 }
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
+  named("ktlint") {
+    resolutionStrategy.eachDependency {
+      if (requested.group == "org.jetbrains.kotlin") {
+        useVersion("2.2.0")
+      }
+    }
+  }
 }
 
 dependencies {
